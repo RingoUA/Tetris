24d222657fdf325f93fe81e8721475140b162faf
Roman Mateichuck <roma.matei4uck@gmail.com>
2023-04-29 23:51:23 +0300

    Added tile resources. Configured bin directory for the build, depending on build type

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-Windows 10:
+#Tetris
+
+This project used learning cmake and conan
+
+##Current build commands for my system:
 
 conan install . --output-folder=build --build=missing
+cmake --preset conan-default
 cd build
 cmake .. -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
 cmake --build . --config Release
---
+++ source/Tetris.cpp
@@ -0,0 +1,24 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Tetris");
+
+    sf::Texture texture;
+    texture.loadFromFile("images/iles.png");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::White);
+        window.display();
+    }
+
+    return 0;
+}
